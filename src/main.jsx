1addb2dfac6ae72be6aa9aac450bba7154e304f0
ragul b <ragul1347@gmail.com>
2025-09-01 09:46:31 +0530

    Fix app.css import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./app.css";
+import "./App.css";
+import "./index.css";
+
+
 
 
 // Bootstrap CSS (and optional JS for components; JS not required for this form)
